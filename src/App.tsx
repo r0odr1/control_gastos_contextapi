1c23fffb326272823b9e56faa758bce5c1aa2e74
rodrigo.bonilla <rodrigo.bonilla@easysoft.com.ec>
2025-12-26 15:24:05 -0500

    reducer para presupuestos

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,9 @@ function App() {
       </header>
 
       <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
-        <BudgetForm />
+        <BudgetForm
+          
+        />
       </div>
     </>
   )
