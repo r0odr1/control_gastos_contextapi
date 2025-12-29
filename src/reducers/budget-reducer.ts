b889640eb5b6239959b655202a1fa8df96cbb149
rodrigo.bonilla <rodrigo.bonilla@easysoft.com.ec>
2025-12-29 10:00:38 -0500

    Accion para cerrar modal

--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -1,7 +1,8 @@
 
 export type BudgetActions =
   { type: 'add-budget', payload: {budget: number} } |
-  { type: 'show-modal'}
+  { type: 'show-modal'} |
+  { type: 'close-modal'}
 
 export type BudgetState = {
   budget: number
@@ -30,6 +31,13 @@ export const budgetReducer = (
     return {
       ...state,
       modal: true
+    }
+  }
+
+  if(action.type === 'close-modal') {
+    return {
+      ...state,
+      modal: false
     }
   }
 
