dcf20bfaab4d5c7ed57cf961d901aec3dcc8ec9f
rodrigo.bonilla <rodrigo.bonilla@easysoft.com.ec>
2025-12-29 14:32:51 -0500

    Elimininando gasto al realizar swipe

--- src/components/ExpenseDetail.tsx
+++ src/components/ExpenseDetail.tsx
@@ -10,6 +10,7 @@ import { formatDate } from "../helpers"
 import type { Expense } from "../types"
 import AmountDisplay from "./AmountDisplay"
 import { categories } from "../data/categories"
+import { useBudget } from "../hooks/useBudget"
 import "react-swipeable-list/dist/styles.css"
 
 type ExpenseDetailProps = {
@@ -17,6 +18,7 @@ type ExpenseDetailProps = {
 }
 
 export default function ExpenseDetail({expense} : ExpenseDetailProps) {
+  const { dispatch } = useBudget()
   const categoryInfo = useMemo (() => categories.filter(cat => cat.id === expense.category)[0], [expense])
 
   const leadingActions = () => (
@@ -32,7 +34,7 @@ export default function ExpenseDetail({expense} : ExpenseDetailProps) {
   const trailingActions = () => (
     <TrailingActions>
       <SwipeAction
-        onClick={() => {}}
+        onClick={() => dispatch({ type: "remove-expense", payload: {id: expense.id} })}
         destructive={true}
       >
         Eliminar
@@ -43,11 +45,11 @@ export default function ExpenseDetail({expense} : ExpenseDetailProps) {
   return (
     <SwipeableList>
       <SwipeableListItem
-        maxSwipe={30}
+        maxSwipe={1}
         leadingActions={leadingActions()}
         trailingActions={trailingActions()}
       >
-        <div className="bg-white shadow-lg p-10 border-gray-200 rounded-lg flex gap-5 items-center">
+        <div className="bg-white shadow-lg p-10 w-full border-gray-200 rounded-lg flex gap-5 items-center">
           <div>
             <img
               src={`/icono_${categoryInfo.icon}.svg`}
--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -5,7 +5,8 @@ export type BudgetActions =
   { type: 'add-budget', payload: {budget: number} } |
   { type: 'show-modal'} |
   { type: 'close-modal'} |
-  { type: 'add-expense', payload: { expense: DraftExpense }}
+  { type: 'add-expense', payload: { expense: DraftExpense }} |
+  { type: 'remove-expense', payload: { id: Expense['id'] }}
 
 export type BudgetState = {
   budget: number
@@ -61,6 +62,13 @@ export const budgetReducer = (
       ...state,
       expenses: [...state.expenses, expense],
       modal: false
+    }
+  }
+
+  if(action.type === 'remove-expense') {
+    return {
+      ...state,
+      expenses: state.expenses.filter( expenses => expenses.id !== action.payload.id)
     }
   }
 
